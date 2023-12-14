cf2bb7215189adc8abdbbccea129383dc182603f
Lenício JR <lenicio.junior@hotmail.com>
2023-12-14 16:48:25 -0300

    Upload Inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<?php
+
+if (!empty($_GET['nome']) && !empty($_GET['idade'])) {
+  $nome = $_GET['nome'];
+  $idade = intval($_GET['idade']);
+
+  if ($idade >= 18) {
+    echo $nome . " Eh maior de idade!";
+  } else {
+    echo $nome . " Eh menor de idade!";
+  }
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="pt">
+
+<head>
+  <meta charset="UTF-8">
+  <title>Página Inicial</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+
+<body>
+
+  <h1>Página Inicial</h1>
+  <form method="get" action="">
+    <label for="nome">Informe o seu nome: </label>
+    <input type="text" name="nome" id="nome" required>
+    <br>
+    <label for="idade">Informe sua idade: </label>
+    <input type="number" name="idade" id="idade" required>
+    <br>
+    <button type="submit">Enviar</button>
+  </form>
+
+</body>
+
+</html>
